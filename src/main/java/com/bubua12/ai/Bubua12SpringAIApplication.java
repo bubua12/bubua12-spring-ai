dc436d56efa260da886fa30bd70d380714bc0af9
wangxl <wangxl@sciyon.com>
2024-05-30 15:36:19 +0800

    feat: initial commit

---
+++ src/main/java/com/bubua12/ai/Bubua12SpringAIApplication.java
@@ -0,0 +1,21 @@
+package com.bubua12.ai;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringAI启动类
+ *
+ * @author bubua12
+ * @since 2024/05/30
+ */
+@Slf4j
+@SpringBootApplication
+public class Bubua12SpringAIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bubua12SpringAIApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bubua12/ai/bubua12springai/Bubua12SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bubua12.ai.bubua12springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bubua12SpringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
